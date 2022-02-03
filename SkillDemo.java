f880cf3f7a5f6208cb9fb5eeb6ee95b064530eda
jackthomas00 <40306951+jackthomas00@users.noreply.github.com>
2022-02-02 20:53:27 -0800

    adding files for first run

---
+++ SkillDemo.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class SkillDemo {
+  public static Integer enumerate(Integer counter) {
+    return counter + 1;
+  }
+
+  public static Integer getNumberFromList(ArrayList<Integer> listOfNums, Integer index) {
+    return listOfNums.get(index);
+  }
+}
---
+++ SkillDemoTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.*;
+
+public class SkillDemoTest {
+  @Test
+  public void enumerateTest() {
+    Integer counter = 10;
+    assertEquals(11, (int) SkillDemo.enumerate(counter));
+  }
+
+  @Test
+  public void getValue() throws IOException {
+    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+    assertEquals(12, (int) SkillDemo.getNumberFromList(numbers, 4));
+  }
+}
